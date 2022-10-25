4c4fbf2e8424184f9dd56bf8fc9bbcb936ef59f9
pyb <350973613@qq.com>
2022-10-25 20:49:31 +0800

    X1 commit

---
+++ src/main/java/com/atguigu/git/GitXTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author pybstart
+ * @create 2022-10-25 20:46
+ */
+public class GitXTest {
+    public static void main(String[] args) {
+        System.out.println("XX");
+    }
+}
