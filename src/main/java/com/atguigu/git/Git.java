dc200795908a9955425f2c39e51efc825b2cab87
pyb <350973613@qq.com>
2022-10-25 20:50:03 +0800

    commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * @author pybstart
+ * @create 2022-10-18 20:33
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("55");
+        System.out.println("666");
+        System.out.println("8888");
+    }
+}
