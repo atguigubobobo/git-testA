a64093da599f48ac48086f2ebd21da10abfb78ce
panyibo <350973613@qq.com>
2023-03-04 23:07:51 +0800

    AAA commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
         System.out.println("gitee test000!");
+        System.out.println("AAA");
     }
 }
